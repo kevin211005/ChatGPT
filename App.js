e8e658efa14920a2abc66a0c8b410cdf6c330339
kevin211005 <86145579+kevin211005@users.noreply.github.com>
2023-06-01 02:30:06 -0700

    Create the view

--- App.js
+++ App.js
@@ -14,38 +14,50 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  ImageBackground
 } from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.viewButtom}>
-          <TouchableOpacity
-            style={styles.sendButton}
-            onPress={() =>{}}
-          >
-            <Text>Search</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+        <ImageBackground 
+          style={styles.backgroundImage}
+          source={{uri:'https://www.vuescript.com/wp-content/uploads/2018/11/Show-Loader-During-Image-Loading-vue-load-image.png'}}
+        >
+          <View style={styles.viewButtom}>
+              <TouchableOpacity
+                style={styles.sendButton}
+                onPress={() =>{}}
+              >
+                <Icon name="microphone" size={30} color="#FFFFFF" />
+              </TouchableOpacity>
+          </View>
+        </ImageBackground>
+
     );
   }
 }
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', 
+    opacity:0.7,
+  },
   container: {
     flex: 1,
   },
   viewButtom: {
     flex: 1,
     justifyContent: 'flex-end',
+    backgroundColor: 'transparent',
+    opacity:1.2,
   },
   sendButton: {
-    backgroundColor: '#00BFFF',
+    backgroundColor: 'darkblue',
     borderRadius: 10,
     padding: 10,
-    margin: 10,
     alignItems: 'center',
   },
 });
